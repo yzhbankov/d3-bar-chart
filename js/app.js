9cf473237e1de95fe39b90596070de9391c0d06f
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2017-01-28 22:15:08 +0200

    add graph visualization

--- js/app.js
+++ js/app.js
@@ -1,53 +1,61 @@
 /**
  * Created by Iaroslav Zhbankov on 27.01.2017.
  */
-var margin = {top: 20, right: 20, bottom: 70, left: 40},
-    width = 600 - margin.left - margin.right,
-    height = 300 - margin.top - margin.bottom;
+var margin = {top: 40, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-// Parse the date / time
-var	parseDate = d3.time.format("%Y-%m").parse;
+function dateParser(date) {
+    var theDate = new Date(date);
+    var year = theDate.getFullYear();
+    var month = theDate.getMonth();
+    return year;
+}
 
-var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
 
-var y = d3.scale.linear().range([height, 0]);
+var y = d3.scale.linear()
+    .range([height, 0]);
 
 var xAxis = d3.svg.axis()
     .scale(x)
-    .orient("bottom")
-    .tickFormat(d3.time.format("%Y-%m"));
+    .orient("bottom");
+
 
 var yAxis = d3.svg.axis()
     .scale(y)
-    .orient("left")
-    .ticks(10);
+    .orient("left");
+
+var tip = d3.tip()
+    .attr('class', 'd3-tip')
+    .offset([-10, 0])
+    .html(function (d) {
+        return "<div><div><strong><h4>$" + d[1] + " Billion</h4></strong></div>" +
+            "<div><h6>" + d[0] + "</h6></div></div>";
+    })
 
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
-    .attr("transform",
-    "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.csv("bar-data.csv", function(error, data) {
+svg.call(tip);
 
-    data.forEach(function(d) {
-        d.date = parseDate(d.date);
-        d.value = +d.value;
-    });
-
-    x.domain(data.map(function(d) { return d.date; }));
-    y.domain([0, d3.max(data, function(d) { return d.value; })]);
+d3.json("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json", function (error, json) {
+    var data = json.data;
+    x.domain(data.map(function (d) {
+        return d[0];
+    }));
+    y.domain([0, d3.max(data, function (d) {
+        return d[1];
+    })]);
 
     svg.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
-        .call(xAxis)
-        .selectAll("text")
-        .style("text-anchor", "end")
-        .attr("dx", "-.8em")
-        .attr("dy", "-.55em")
-        .attr("transform", "rotate(-90)" );
+        .call(xAxis);
 
     svg.append("g")
         .attr("class", "y axis")
@@ -57,15 +65,30 @@ d3.csv("bar-data.csv", function(error, data) {
         .attr("y", 6)
         .attr("dy", ".71em")
         .style("text-anchor", "end")
-        .text("Value ($)");
+        .text("Frequency");
 
-    svg.selectAll("bar")
+
+    svg.selectAll(".bar")
         .data(data)
         .enter().append("rect")
-        .style("fill", "steelblue")
-        .attr("x", function(d) { return x(d.date); })
+        .attr("class", "bar")
+        .attr("x", function (d) {
+            return x(d[0]);
+        })
         .attr("width", x.rangeBand())
-        .attr("y", function(d) { return y(d.value); })
-        .attr("height", function(d) { return height - y(d.value); });
+        .attr("y", function (d) {
+            return y(d[1]);
+        })
+        .attr("height", function (d) {
+            return height - y(d[1]);
+        })
+        .on('mouseover', tip.show)
+        .on('mouseout', tip.hide)
+
 
 });
+
+function type(d) {
+    d[1] = +d[1];
+    return d;
+}
