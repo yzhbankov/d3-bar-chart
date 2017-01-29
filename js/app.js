6d92cad9ee745f7f62bdc56eb316f5dc7a2f1ad1
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2017-01-29 16:15:55 +0200

    improve graph layout

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /**
  * Created by Iaroslav Zhbankov on 27.01.2017.
  */
-var margin = {top: 40, right: 20, bottom: 30, left: 40},
+var margin = {top: 40, right: 20, bottom: 60, left: 80},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
@@ -12,7 +12,22 @@ function dateParser(date) {
     return year;
 }
 
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json', false);
+xhr.send();
+if (xhr.status != 200) {
+    alert(xhr.status + ': ' + xhr.statusText);
+} else {
+    var response = JSON.parse(xhr.responseText);
+}
+
+var data = response.data;
+
 var x = d3.scale.ordinal()
+    .domain(data.map(function (d) {
+        d[0] = dateParser(d[0]);
+        return d[0]
+    }))
     .rangeRoundBands([0, width], .1);
 
 var y = d3.scale.linear()
@@ -21,8 +36,9 @@ var y = d3.scale.linear()
 var xAxis = d3.svg.axis()
     .scale(x)
     .orient("bottom")
-    .ticks(d3.time.years, 2);
-
+    .tickValues(x.domain().filter(function (d, i) {
+        return !(i % 5);
+    }));
 
 var yAxis = d3.svg.axis()
     .scale(y)
@@ -36,7 +52,7 @@ var tip = d3.tip()
             "<div><h6>" + d[0] + "</h6></div></div>";
     });
 
-var svg = d3.select("body").append("svg")
+var svg = d3.select(".bar-chart").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
@@ -44,45 +60,52 @@ var svg = d3.select("body").append("svg")
 
 svg.call(tip);
 
-d3.json("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json", function (error, json) {
-    var data = json.data;
-    x.domain(data.map(function (d) {
-        return d[0];
-    }));
-    y.domain([0, d3.max(data, function (d) {
-        return d[1];
-    })]);
+x.domain(data.map(function (d) {
+    return d[0];
+}));
+y.domain([0, d3.max(data, function (d) {
+    return d[1];
+})]);
 
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
+svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis)
+    .append('text')
+    .attr("dy", "3.71em")
+    .attr("dx", "9em")
+    .text("Units: Billions of Dollars Seasonal Adjustment:" +
+    " Seasonally Adjusted Annual Rate Notes");
 
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("Frequency");
+svg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis)
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
+    .text("Gross Domestic Product, USA");
 
 
-    svg.selectAll(".bar")
-        .data(data)
-        .enter().append("rect")
-        .attr("class", "bar")
-        .attr("x", function (d) {
-            return x(d[0]);
-        })
-        .attr("width", x.rangeBand())
-        .attr("y", function (d) {
-            return y(d[1]);
-        })
-        .attr("height", function (d) {
-            return height - y(d[1]);
-        })
-        .on('mouseover', tip.show)
-        .on('mouseout', tip.hide)
-});
+svg.selectAll(".bar")
+    .data(data)
+    .enter().append("rect")
+    .attr("class", "bar")
+    .attr("x", function (d) {
+        return x(d[0]);
+    })
+    .attr("width", x.rangeBand())
+    .attr("y", function (d) {
+        return y(d[1]);
+    })
+    .attr("height", function (d) {
+        return height - y(d[1]);
+    })
+    .on('mouseover', tip.show)
+    .on('mouseout', tip.hide)
+
+svg.append("text")
+    .attr("class", "title")
+    .attr("dx", "8em")
+    .text("Gross Domestic Product");
